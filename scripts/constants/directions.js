fe2869672ce8343cfe15c9ea00dbf1ac94d2a3bb
Hayk Shahinyan <shahinyan142@gmail.com>
2025-10-06 16:06:23 +0400

    replaces docs to dist

--- scripts/constants/directions.js
+++ scripts/constants/directions.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const srcDir = path.join(__dirname, '../../src');
-const distDir = path.join(__dirname, '../../docs');
+const distDir = path.join(__dirname, '../../dist');
 const localesDir = path.join(__dirname, "../../locales");
 
 module.exports = {
